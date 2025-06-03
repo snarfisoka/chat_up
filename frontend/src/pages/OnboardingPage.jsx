ead6cd7d12a0e076a4f027109fe027efcbeaf6c9
snarfisoka <snarfisoka@gmail.com>
2025-06-03 20:30:43 +0800

    update frontend

---
+++ frontend/src/pages/OnboardingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OnboardingPage = () => {
+  return (
+    <div>
+      OnboardingPage
+      <button>hello</button>
+    </div>
+  )
+}
+
+export default OnboardingPage

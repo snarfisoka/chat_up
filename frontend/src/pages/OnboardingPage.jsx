6e7302f0d981e83ce79059451f6e86286e14708f
snarfisoka <snarfisoka@gmail.com>
2025-06-08 02:23:18 +0800

    changes on onboard page

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -24,12 +24,22 @@ const OnboardingPage = () => {
       toast.success("Profile onboarded successfully");
       queryClient.invalidateQueries({ queryKey: ["authUser" ]});
     }
-  })
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onboardingMutation(formState)
+  }
 
   return (
-    <div>
-      OnboardingPage
-      <button>hello</button>
+    <div className='min-h-screen bg-base-100 flex items-center justify-center p-4'>
+      <div className='card bg-base-200 w-full max-w-3xl shadow-xl'>
+        <div className='card-body p-6 sm:p-8'>
+          <h1 className='text-2xl sm:text-3xl font-bold text-center mb-6'>Complete Your Profile</h1>
+
+        </div>
+      </div>
     </div>
   )
 }

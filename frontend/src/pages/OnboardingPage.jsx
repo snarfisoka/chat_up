4c74e9e7dec5e105a2cac5c067b122113d882300
snarfisoka <snarfisoka@gmail.com>
2025-06-09 01:59:20 +0800

    new update on onboarding page

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import useAuthUser from '../hooks/useAuthUser'
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { ShuffleIcon } from 'lucide-react';
 
 import toast from 'react-hot-toast';
+import { completeOnboarding } from '../lib/api';
 
 const OnboardingPage = () => {
 
@@ -32,12 +34,39 @@ const OnboardingPage = () => {
     onboardingMutation(formState)
   }
 
+  const handleRandomAvatar = () => {}
+
   return (
     <div className='min-h-screen bg-base-100 flex items-center justify-center p-4'>
       <div className='card bg-base-200 w-full max-w-3xl shadow-xl'>
         <div className='card-body p-6 sm:p-8'>
           <h1 className='text-2xl sm:text-3xl font-bold text-center mb-6'>Complete Your Profile</h1>
 
+          <form onSubmit={handleSubmit} className='space-y-6'></form>
+          {/* PROFILE PIC CONTAINER */}   
+          <div className='flex flex-col items-center justify-center space-y-4'>
+            {/* IMAGE PREVIEW */}
+            <div className='size-32 rounded-full bg-base-300 overflow-hidden'>
+              {formState.profilePic ? (
+                <img
+                src={formState.profilePic}
+                atl="Profile Preview"
+                className='w-full h-full object-cover' 
+                />
+              ) : (
+                <div className='flex items-center justify-center h-full'>
+                  <CameraIcon className="size-12 text-base-content opacity-40" />
+                </div>
+              )}
+            </div>
+            {/* Generate Random Avatar BTN */}
+            <div className='flex items-center gap-2'>
+              <button type='button' onClick={handleRandomAvatar} className='btn btn-accent'>
+                <ShuffleIcon className='size-4 mr-2' />
+                Generate Random Avatar
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
